09f60723e548dede4bb3e953503fa1700c0f5813
Penguin-SAMA <496107146@qq.com>
2023-09-19 20:42:38 +0800

    Update

--- oneWeekend/src/main.cpp
+++ oneWeekend/src/main.cpp
@@ -1,21 +1,35 @@
+#include "rtweekend.h"
+
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
 
-bool hit_sphere(const point3 &center, double radius, const ray &r) {
-    vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto delta = b * b - 4 * a * c;
-    return (delta >= 0);
-}
+// double hit_sphere(const point3 &center, double radius, const ray &r) {
+//     vec3 oc = r.origin() - center;
+//     // auto a = dot(r.direction(), r.direction());
+//     // auto b = 2 * dot(oc, r.direction());
+//     // auto c = dot(oc, oc) - radius * radius;
+//     auto a = r.direction().length_squared();
+//     auto half_b = dot(oc, r.direction());
+//     auto c = oc.length_squared() - radius * radius;
+//     auto delta = half_b * half_b - a * c;
 
-color ray_color(const ray &r) {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
-        return color(1, 0, 0);
+//     if (delta < 0) {
+//         return -1.0;
+//     } else {
+//         // 只返回与光线更近的交点，所以不需要-b+delta
+//         return (-half_b - sqrt(delta)) / a;
+//     }
+// }
+
+color ray_color(const ray &r, const hittable &world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
+    }
 
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
@@ -29,6 +43,12 @@ int main() {
 
     int image_height = static_cast<int>(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+
+    // World
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
     auto focal_length = 1.0;
@@ -60,7 +80,7 @@ int main() {
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
