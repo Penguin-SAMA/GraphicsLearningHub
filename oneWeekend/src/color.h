493b1d646f6c481d3eb1baa432e4f5214ea67ff4
Penguin-SAMA <496107146@qq.com>
2023-10-10 21:29:25 +0800

    metal update

--- oneWeekend/src/color.h
+++ oneWeekend/src/color.h
@@ -1,15 +1,18 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-#include "vec3.h"
 #include "interval.h"
+#include "vec3.h"
 
 #include <iostream>
 
 using color = vec3;
 
-void write_color(std::ostream &out, color pixel_color, int samples_per_pixel)
-{
+inline double linear_to_gamma(double linear_component) {
+    return sqrt(linear_component);
+}
+
+void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
@@ -18,6 +21,10 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel)
     r *= scale;
     g *= scale;
     b *= scale;
+
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     static const interval intensity(0.000, 0.999);
     out << static_cast<int>(256 * intensity.clamp(r)) << ' '
--- oneWeekend/src/sphere.h
+++ oneWeekend/src/sphere.h
@@ -4,17 +4,12 @@
 #include "hittable.h"
 #include "vec3.h"
 
-class sphere : public hittable
-{
-public:
-    sphere(point3 _center, double _radius)
-    : center(_center)
-    , radius(_radius)
-    {
-    }
+class sphere : public hittable {
+  public:
+    sphere(point3 _center, double _radius, std::shared_ptr<material> _material)
+        : center(_center), radius(_radius), mat(_material) {}
 
-    bool hit(const ray &r, interval ray_t, hit_record &rec) const override
-    {
+    bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
         vec3 oc = r.origin() - center;
         // auto a = dot(r.direction(), r.direction());
         // auto b = 2.0 * dot(oc, r.direction());
@@ -30,11 +25,9 @@ public:
         auto sqrtd = sqrt(discriminant);
 
         auto root = (-half_b - sqrtd) / a;
-        if (!ray_t.surrounds(root))
-        {
+        if (!ray_t.surrounds(root)) {
             root = (-half_b + sqrtd) / a;
-            if (!ray_t.surrounds(root))
-            {
+            if (!ray_t.surrounds(root)) {
                 return false;
             }
         }
@@ -43,13 +36,15 @@ public:
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
 
-private:
+  private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 #endif
