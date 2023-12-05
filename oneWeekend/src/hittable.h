553ba606327e2181554ad241663773e3469f0030
Penguin-SAMA <496107146@qq.com>
2023-12-05 20:53:05 +0800

    complete ch10

--- oneWeekend/src/hittable.h
+++ oneWeekend/src/hittable.h
@@ -3,13 +3,17 @@
 
 #include "interval.h"
 #include "ray.h"
+#include "rtweekend.h"
+
+class material;
 
 class hit_record {
 public:
-    point3 p;
-    vec3   normal;
-    double t;
-    bool   front_face;
+    point3               p;
+    vec3                 normal;
+    shared_ptr<material> mat;
+    double               t;
+    bool                 front_face;
 
     void set_face_normal(const ray &r, const vec3 &outward_normal) {
         front_face = dot(r.direction(), outward_normal);
